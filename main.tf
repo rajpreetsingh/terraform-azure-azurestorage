9cc2906265452d81425198c0a56a02f1ae8ca719
Raj Singh <rajpreet@hotmail.co.nz>
2022-10-30 10:10:22 +1300

    test

---
+++ main.tf
@@ -0,0 +1,55 @@
+resource "random_string" "rand" {
+  length    = 5
+  special   = false
+  upper     = false
+}
+
+resource "azurerm_resource_group" "default" {
+  name      = var.resource_group
+  location  = var.location
+}
+
+resource "azurerm_storage_account" "storage_account" {
+  name                        = "${var.storage_account}${random_string.rand.result}"
+  resource_group_name         = azurerm_resource_group.default.name
+  location                    = azurerm_resource_group.default.location
+  account_tier                = "Standard"
+  account_replication_type    = "LRS"
+}
+
+# resource "azurerm_storage_container" "storage_container" {
+#     name                    = "serverless"
+#     storage_account_name    = azurerm_storage_account.storage_account.name
+#     container_access_type   = "private"
+# }
+
+# data "azurerm_storage_account_sas" "storage_sas" {
+#     connection_string = azurerm_storage_account.storage_account.primary_connection_string
+
+#     resource_types {
+#         service   = false
+#         container = false
+#         object    = false
+#     }
+
+#     services {
+#         blob    = true
+#         queue   = false
+#         table   = false
+#         file    = false
+#     }
+
+#     start   = "2022-06-19T00:00:00Z"
+#     expiry  = "2025-06-19T00:00:00Z"
+
+#     permissions {
+#         read    = true
+#         write   = false
+#         delete  = false
+#         list    = false
+#         add     = false
+#         create  = false
+#         update  = false
+#         process = false
+#     }
+# }
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "location" {
+    type = string
+    default = "australiasoutheast"
+}
+
+variable "resource_group" {
+    type = string
+    default = "terraform-learn"
+}
+
+variable "storage_account" {
+    type = string
+    default = "terraform"
+}
